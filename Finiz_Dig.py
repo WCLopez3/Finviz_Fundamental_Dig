68ed5d2c051308836dcb82e04705c8261a661c2a
WCLopez3 <98786953+WCLopez3@users.noreply.github.com>
2022-05-09 16:58:30 -0400

    Update and rename Dig.py to Finiz_Dig.py

--- Finiz_Dig.py
+++ Finiz_Dig.py
@@ -18,12 +18,13 @@ def finviz_open(event=None):
     driver.minimize_window()
     driver.get('https://finviz.com/')
 
-
+    # Searching for the input box for finviz.com
     search_box = driver.find_element(By.XPATH, '//*[@id="search"]/div/form/input')
     search_box.send_keys(ticker_entry.get())
     search_box.send_keys(Keys.RETURN)
 
     try:
+        # Waiting for the table to appear 
         snapshot = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'snapshot-table2')))
         marketcap = snapshot.find_element(By.XPATH, '/html/body/div[4]/div/table[2]/tbody/tr[2]/td[2]/b').text
         shrsfloat = snapshot.find_element(By.XPATH,'/html/body/div[4]/div/table[2]/tbody/tr[2]/td[10]/b').text
@@ -35,22 +36,25 @@ def finviz_open(event=None):
         SSR = float(prevclose) * .9
         time.sleep(1)
 
-        # Time search was initiated
+        # Date and Time format M/D/YYYY H:M:S
         now = datetime.now()
         date_string = now.strftime("%m/%d/%y %H:%M:%S")
-        print(date_string)
+        #print(date_string)
 
 
-        # Create a Python dictionary
+        # Creating a Python dictionary
         data = {'': [date_string, marketcap, shrsfloat, instown, ss, avgvol, prevclose, SSR],
 
                 }
+        # Creating the dataframe of above data
         df = pd.DataFrame(data)
         df.index = [ticker_entry.get(), 'Market Cap:', 'Shrs Float:', 'Inst Own:', 'Short Float', 'Avg Volume:', 'Prev Close:', 'SSR:']
         #df.index.name = ticker_entry.get()
         df_str = str(df)
         txtbox.insert(0.0, df_str)
-        df.to_csv('fundies.csv')
+        
+        # Append data frame to CSV file
+        df.to_csv('fundies.csv', moda='a')
         
         print(df)
         time.sleep(1)
@@ -75,9 +79,11 @@ def del_entry(event):
 buttons_frame = Frame(master_window)
 buttons_frame.grid(row=0, column=0, sticky=W+E)
 
+# Search Button
 search_but = Button(buttons_frame, text='Search', command = finviz_open)
 search_but.grid(row=0, column=1)
 
+# Input box for the Ticker
 v = StringVar()
 ticker_entry = Entry(buttons_frame, textvariable=v)
 ticker_entry.grid(row = 0, column = 0, padx = 10, pady = 10)
@@ -86,7 +92,7 @@ ticker_entry.bind('<Button-1>', del_entry)
 ticker_entry.bind('<KeyRelease>', caps)
 ticker_entry.bind('<Return>', finviz_open)
 
-# WindowFrame ----------------------------------------------------
+# WindowFrame 
 group1 = LabelFrame(master_window, text=" The Process ", padx=5, pady=5)
 group1.grid(row=1, column=0, columnspan=3, padx=2, pady=0, sticky=E+W+N+S)
 
